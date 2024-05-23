07e97b0e4dc5e6014798ccd2e82af908d0fd52e5
dbsDevelops <alu.129152@usj.es>
2024-05-23 13:15:33 +0200

    Removed enableEdgeToEdge

--- app/src/main/java/com/example/weatherapp_mobapp/BaseCommunityActivity.kt
+++ app/src/main/java/com/example/weatherapp_mobapp/BaseCommunityActivity.kt
@@ -22,12 +22,6 @@ abstract class BaseCommunityActivity : AppCompatActivity() {
     private var currentInitDate = sdf.format(Date())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_base_community)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
