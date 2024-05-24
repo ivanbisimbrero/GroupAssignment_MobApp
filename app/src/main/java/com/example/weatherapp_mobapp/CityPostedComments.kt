a40308b88804921b4250ced65fa2e298d94b85fd
ivanbisimbrero <alu.135046@usj.es>
2024-05-24 12:09:24 +0200

    Changed buttons to Image buttons and moved to the top, to avoid conflicts with space at the botton, with historical data.
    Co-authored-by: dbsDevelops <alu.129152@usj.es>

--- app/src/main/java/com/example/weatherapp_mobapp/CityPostedComments.kt
+++ app/src/main/java/com/example/weatherapp_mobapp/CityPostedComments.kt
@@ -28,7 +28,6 @@ import java.util.Date
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import androidx.core.content.ContextCompat
 
 
 class CityPostedComments : AppCompatActivity() {
@@ -38,7 +37,6 @@ class CityPostedComments : AppCompatActivity() {
     private lateinit var commentAdapter: CommentAdapter
     private var isEditing = false
     private val sdf = SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
-    private var currentInitDate = sdf.format(Date())
     private val repository: CrudAPI by lazy {
         SharedPreferencesRepository(
             application.getSharedPreferences(
@@ -51,6 +49,7 @@ class CityPostedComments : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
+
         //Setup database reference
         val cityName = intent.getStringExtra("cityName")!!
         view.comments.tvModelCityName.text = cityName
@@ -68,59 +67,11 @@ class CityPostedComments : AppCompatActivity() {
         view.comments.rvChatMessages.layoutManager = LinearLayoutManager(this)
         commentAdapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
             override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                //When a new item is inserted in the adapter, it autoscrolls down, like in a real chat
                 super.onItemRangeInserted(positionStart, itemCount)
                 view.comments.rvChatMessages.layoutManager!!.scrollToPosition(commentAdapter.itemCount - 1)
             }
         })
-
-        // Setup swipe to delete
-        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                return false
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val position = viewHolder.adapterPosition
-                val comment = commentAdapter.commentsList[position]
-                if (comment.isCurrentUser){
-                    deleteComment(comment)
-                }else{
-                    commentAdapter.notifyDataSetChanged()
-                    Toast.makeText(this@CityPostedComments, "You can only delete your own comments", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            override fun onChildDraw(
-                c: Canvas,
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                dX: Float,
-                dY: Float,
-                actionState: Int,
-                isCurrentlyActive: Boolean
-            ) {
-                val itemView = viewHolder.itemView
-                val background = Paint().apply { color = Color.RED }
-
-                if  (dX < 0) { // Swiping to the left
-                    c.drawRect(
-                        itemView.right + dX,
-                        itemView.top.toFloat(),
-                        itemView.right.toFloat(),
-                        itemView.bottom.toFloat(),
-                        background
-                    )
-                }
-                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-            }
-        }
-        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
-        itemTouchHelper.attachToRecyclerView(view.comments.rvChatMessages)
-
 
         // Change the button text
         if (DataUtils.mainUser.name.isEmpty() && DataUtils.mainUser.email.isEmpty()){
@@ -157,6 +108,7 @@ class CityPostedComments : AppCompatActivity() {
                 Toast.makeText(this, "Please, set up an email and username", Toast.LENGTH_SHORT)
                     .show()
             } else if(view.sendComment.etComments.text.toString().isNotEmpty()) run {
+                //We format the date in order to be the same for all users
                 val currentDate = sdf.format(Date())
                 println(currentDate)
                 val newMessageKey = dbReference.push().key!!
@@ -174,6 +126,7 @@ class CityPostedComments : AppCompatActivity() {
             }
         }
         view.comments.btnChange.setOnClickListener {
+            //As we use the ";" in the parser in shared preferences, we deny the use of the character.
             if(view.comments.etChatEmail.text.toString().contains(";") ||
                 view.comments.etChatUsername.text.toString().contains(";")) {
                 Toast.makeText(this, "Ilegal character -> ;. Remove it!", Toast.LENGTH_SHORT).show()
@@ -196,7 +149,11 @@ class CityPostedComments : AppCompatActivity() {
                 // Change the button text
                 if (DataUtils.mainUser.name.isEmpty() && DataUtils.mainUser.email.isEmpty()){
                     view.comments.btnChange.text = "Login"
+                    //If the user needs to login, we show him the fields
+                    view.comments.etChatUsername.visibility = View.VISIBLE
+                    view.comments.etChatEmail.visibility = View.VISIBLE
                 } else {
+                    //If he is currently logged in, we show him "Change Username"
                     view.comments.btnChange.text = "Change Username"
                 }
 
@@ -233,8 +190,9 @@ class CityPostedComments : AppCompatActivity() {
         dbReference.addChildEventListener(object : ChildEventListener {
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
                 val comment = snapshot.getValue(Comment::class.java)
-                //Check if the email is the same as the user one and that the comment isn't yet in the adapter
+                //Check that the comment isn't yet in the adapter
                 if(commentAdapter.commentsList.none { it.id == comment!!.id }){
+                    //If the user has the same email as the comment, we suppose he is the author and that he can remove it
                     if(comment!!.email == DataUtils.mainUser.email) {
                         comment.isCurrentUser = true
                         commentAdapter.insertNewComment(comment)
@@ -247,6 +205,7 @@ class CityPostedComments : AppCompatActivity() {
 
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
             override fun onChildRemoved(snapshot: DataSnapshot) {
+                //We retrieve the removed comment, and we use the adapter method removeComment to remove it
                 val comment = snapshot.getValue(Comment::class.java)
                 if (comment != null) {
                     commentAdapter.removeComment(comment)
@@ -256,11 +215,57 @@ class CityPostedComments : AppCompatActivity() {
             override fun onCancelled(error: DatabaseError) {}
         })
 
-    }
+        //Setup swipe to delete
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
 
-    override fun onResume() {
-        super.onResume()
-        currentInitDate = sdf.format(Date())
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                //We get the item position in order to delete in when the user has swiped the comment
+                val position = viewHolder.adapterPosition
+                val comment = commentAdapter.commentsList[position]
+                if (comment.isCurrentUser){
+                    deleteComment(comment)
+                }else{
+                    commentAdapter.notifyItemChanged(position)
+                    Toast.makeText(this@CityPostedComments, "You can only delete your own comments", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                //We paint with red the swipe
+                val itemView = viewHolder.itemView
+                val background = Paint().apply { color = Color.RED }
+
+                if  (dX < 0) { // Swiping to the left
+                    c.drawRect(
+                        itemView.right + dX,
+                        itemView.top.toFloat(),
+                        itemView.right.toFloat(),
+                        itemView.bottom.toFloat(),
+                        background
+                    )
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+        }
+        //Finally, we add the action swipe to delete to the recycler view
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(view.comments.rvChatMessages)
+
     }
 
     private fun haveEmptyUsernameOrEmail(): Boolean {
@@ -268,6 +273,7 @@ class CityPostedComments : AppCompatActivity() {
     }
 
     private fun deleteComment(comment: Comment) {
+        //We get the item with his id, ann we try to remove it. If success, we remove it from the adapter
         dbReference.child(comment.id).removeValue().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 commentAdapter.removeComment(comment)
